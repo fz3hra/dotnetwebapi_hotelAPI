051e1572e27788bc00e6fb8280b71cc766850299
fz3hra <mujorefaatimah@gmail.com>
2023-02-04 23:35:19 +0400

    :sparkles: recommit - implementation of scaffolding and dtos

---
+++ HotelApi/Controllers/CountriesController.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HotelApi.Data;
+using HotelApi.ModelDtos.countryDtos;
+
+namespace HotelApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        private readonly HotelDbContext _context;
+
+        public CountriesController(HotelDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Countries
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Country>>> Getcountries()
+        {
+            if (_context.countries == null)
+            {
+                return NotFound();
+            }
+            return await _context.countries.ToListAsync();
+        }
+
+        // GET: api/Countries/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Country>> GetCountry(int id)
+        {
+            if (_context.countries == null)
+            {
+                return NotFound();
+            }
+            var country = await _context.countries.FindAsync(id);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return country;
+        }
+
+        // PUT: api/Countries/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCountry(int id, Country country)
+        {
+            if (id != country.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(country).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CountryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Countries
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Country>> PostCountry(CreateCountryDto createCountry)
+        {
+            var country = new Country
+            {
+                Name = createCountry.Name,
+                shortName = createCountry.ShortName
+            };
+
+            if (_context.countries == null)
+            {
+                return Problem("Entity set 'HotelDbContext.countries'  is null.");
+            }
+            _context.countries.Add(country);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCountry", new { id = country.Id }, country);
+        }
+
+        // DELETE: api/Countries/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCountry(int id)
+        {
+            if (_context.countries == null)
+            {
+                return NotFound();
+            }
+            var country = await _context.countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            _context.countries.Remove(country);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CountryExists(int id)
+        {
+            return (_context.countries?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
